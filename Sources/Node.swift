6fcfe844d99968507c435e8a1dcc673378dc5de6
Shun Takebayashi <shun@takebayashi.asia>
2016-09-06 06:55:30 +0900

    Remove parent var from OpaqueNode

--- Sources/Node.swift
+++ Sources/Node.swift
@@ -1,35 +1,21 @@
 import CGumbo
 
 public protocol OpaqueNode {
-    var parent: OpaqueNode? { get }
 }
 
 public protocol Node: OpaqueNode {
     associatedtype RawType
-    init(rawNode: RawType, parent: OpaqueNode?)
+    init(rawNode: RawType)
 }
 
 fileprivate func mapPointerToStruct(rawNode: UnsafeMutablePointer<GumboNode>) -> OpaqueNode {
-    var parent: OpaqueNode? = nil
-    if rawNode.pointee.parent != nil {
-        parent = mapPointerToStruct(rawNode: rawNode.pointee.parent)
-    }
     switch rawNode.pointee.type {
     case GUMBO_NODE_DOCUMENT:
-        return DocumentNode(
-            rawNode: rawNode.pointee.v.document,
-            parent: parent
-        )
+        return DocumentNode(rawNode: rawNode.pointee.v.document)
     case GUMBO_NODE_ELEMENT:
-        return ElementNode(
-            rawNode: rawNode.pointee.v.element,
-            parent: parent
-        )
+        return ElementNode(rawNode: rawNode.pointee.v.element)
     case GUMBO_NODE_TEXT:
-        return TextNode(
-            rawNode: rawNode.pointee.v.text,
-            parent: parent
-        )
+        return TextNode(rawNode: rawNode.pointee.v.text)
     default:
         fatalError()
     }
@@ -40,11 +26,9 @@ public typealias TagType = GumboTag
 public struct DocumentNode: Node {
     public typealias RawType = GumboDocument
 
-    public let parent: OpaqueNode?
     public let children: [OpaqueNode]
 
-    public init(rawNode: GumboDocument, parent: OpaqueNode?) {
-        self.parent = parent
+    public init(rawNode: GumboDocument) {
         self.children = rawNode.children
             .flatMap { $0?.assumingMemoryBound(to: GumboNode.self) }
             .map(mapPointerToStruct)
@@ -54,13 +38,11 @@ public struct DocumentNode: Node {
 public struct ElementNode: Node {
     public typealias RawType = GumboElement
 
-    public let parent: OpaqueNode?
     public let children: [OpaqueNode]
     public let tag: TagType
     public let attributes: [Attribute]
 
-    public init(rawNode: GumboElement, parent: OpaqueNode?) {
-        self.parent = parent
+    public init(rawNode: GumboElement) {
         self.children = rawNode.children
             .flatMap { $0?.assumingMemoryBound(to: GumboNode.self) }
             .map(mapPointerToStruct)
@@ -74,11 +56,9 @@ public struct ElementNode: Node {
 public struct TextNode: Node {
     public typealias RawType = GumboText
 
-    public let parent: OpaqueNode?
     public let text: String
 
-    public init(rawNode: GumboText, parent: OpaqueNode?) {
-        self.parent = parent
+    public init(rawNode: GumboText) {
         self.text = String(cString: rawNode.text)
     }
 }
