b0f4e581c0b2c954a9562dfe04fabc4ce66299fc
Shun Takebayashi <shun@takebayashi.asia>
2016-09-11 22:01:12 +0900

    Add Attribute.valuesSeparatedByWhiteSpaces

--- Sources/Attribute.swift
+++ Sources/Attribute.swift
@@ -9,3 +9,31 @@ public struct Attribute {
         self.value = String(cString: rawNode.value)
     }
 }
+
+extension Attribute {
+    public var valuesSeparatedByWhiteSpaces: [String] {
+        get {
+            // https://www.w3.org/TR/html5/infrastructure.html#space-character
+            // The space characters, for the purposes of this specification,
+            // are U+0020 SPACE, "tab" (U+0009), "LF" (U+000A), "FF" (U+000C),
+            // and "CR" (U+000D).
+            let chunks = self.value.characters.split { c in
+                switch c {
+                case Character(UnicodeScalar(0x20)):
+                    return true
+                case Character(UnicodeScalar(0x09)):
+                    return true
+                case Character(UnicodeScalar(0x0a)):
+                    return true
+                case Character(UnicodeScalar(0x0c)):
+                    return true
+                case Character(UnicodeScalar(0x0d)):
+                    return true
+                default:
+                    return false
+                }
+            }
+            return chunks.map(String.init)
+        }
+    }
+}
